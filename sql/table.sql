5ccbac7b8de45024a138207bbcd4f0d78f9a9a85
Lucas Burns <burnsac@me.com>
2021-02-18 21:18:20 -0600

    table for sql, deleted dollar signs to use as int

--- sql/table.sql
+++ sql/table.sql
@@ -1,16 +1,14 @@
-CREATE TABLE crypto.cryptousers
-  (
-     id         INT NOT NULL auto_increment,
-     documentid INT NOT NULL,
-     platform   TEXT NOT NULL,
-     date       DATETIME NOT NULL,
-     type       TEXT NOT NULL,
-     asset      TEXT NOT NULL,
-     quantity   TEXT NOT NULL,
-     costbasis  TEXT NOT NULL,
-     subtotal   TEXT NOT NULL,
-     total      TEXT NOT NULL,
-     amountleft TEXT NOT NULL,
-     PRIMARY KEY (id)
-  )
-engine = innodb;
+CREATE TABLE crypto.cryptoUsers (
+  ID INT NOT NULL AUTO_INCREMENT,
+  documentID INT NOT NULL,
+  platform TEXT NOT NULL,
+  date DATETIME NOT NULL,
+  type TEXT NOT NULL,
+  asset TEXT NOT NULL,
+  quantity TEXT NOT NULL,
+  costBasis TEXT NOT NULL,
+  subtotal TEXT NOT NULL,
+  total TEXT NOT NULL,
+  amountLeft TEXT NOT NULL,
+  PRIMARY KEY (ID)
+) ENGINE = InnoDB;
